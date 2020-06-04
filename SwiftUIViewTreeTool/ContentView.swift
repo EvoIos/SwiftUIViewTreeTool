24b4cf01b712da4ce3556f5b3abef9961b8d4918
zhenglanchun <zhenglanchun@windimg.com>
2020-06-04 20:45:43 +0800

    add project

---
+++ SwiftUIViewTreeTool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIViewTreeTool
+//
+//  Created by zlc on 2020/6/4.
+//  Copyright © 2020 open.tool.app. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
